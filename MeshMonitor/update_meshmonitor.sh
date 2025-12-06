e7586f0adf03ee3071b4bbddd66cbfa2f35f930a
Michael A. Cojocari <michael.cojocari@gmail.com>
2025-12-05 19:29:57 -0500

    Stuff

--- MeshMonitor/update_meshmonitor.sh
+++ MeshMonitor/update_meshmonitor.sh
@@ -1,4 +1,5 @@
 sudo docker stop meshmonitor
 sudo docker rm meshmonitor
 sudo docker pull ghcr.io/yeraze/meshmonitor:latest
-docker run -d --env-file ./meshmonitor.env --name meshmonitor -p 9090:3001 -v meshmonitor-data:/data ghcr.io/yeraze/meshmonitor:latest
+# docker run -d --env-file ./meshmonitor.env --name meshmonitor -p 9090:3001 -v meshmonitor-data:/data ghcr.io/yeraze/meshmonitor:latest
+docker compose up -d
